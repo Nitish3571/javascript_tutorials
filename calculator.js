160f37d7b2f0b3531ba5a6c0e242d4fc13fc04fe
Nitish <nitishkumar4042004@gmail.com>
2023-08-28 02:52:26 +0530

    calculater

--- calculator.js
+++ calculator.js
@@ -6,10 +6,6 @@ let arr = Array.from(btn);
 btn.forEach(function (button){
     // console.log(button);
     button.addEventListener('click' , (e) => {
-        // console.log(e.target);
-        // const val = e.target.innerHTML;
-        // inputVal.innerHTML = val;
-        // console.log(inputVal);
         if(e.target.innerHTML == '='){
             string = eval(string);
             // console.log(string);
@@ -17,7 +13,9 @@ btn.forEach(function (button){
         }else if(e.target.innerHTML == 'AC'){
             string = '';
             inputVal.value = string;
-
+        }else if(e.target.innerHTML == 'DEL'){
+            string  =string.substring(0 , string.length-1);
+            inputVal.value = string;
         }
         else{
         string +=e.target.innerHTML;
